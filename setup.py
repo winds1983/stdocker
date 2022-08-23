998fabe6735da46ab190aadcba0db0b4cffc14cb
Jason Sun <dev.winds@gmail.com>
2022-08-23 11:49:22 +0800

    Fix bugs and improve setup

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup, find_packages
 
+def read_files(files):
+    data = []
+    for file in files:
+        with open(file, encoding='utf-8') as f:
+            data.append(f.read())
+    return "\n".join(data)
+
+long_description = read_files(['README.md', 'CHANGELOG.md'])
+
+requirements = []
 with open('requirements.txt') as f:
     requirements = f.readlines()
 
-long_description = 'A more intelligent, user-friendly and advanced configurable local development environment built with Docker.'
+meta = {}
+with open('./stdocker/version.py', encoding='utf-8') as f:
+    exec(f.read(), meta)
 
 setup(
     name='stdocker',
-    version='1.0.0',
+    version=meta['__version__'],
     author='Jason Sun',
     author_email='sunf@shinetechsoftware.com',
     url='https://github.com/winds1983/stdocker',
@@ -25,12 +37,12 @@ setup(
             'stdocker=stdocker.cli:main'
         ]
     },
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
-    keywords='python package stdocker shinetech docker',
+    ],
+    keywords=['python package', 'stdocker', 'shinetech', 'docker', 'development environment'],
     install_requires=requirements,
     zip_safe=False
 )
