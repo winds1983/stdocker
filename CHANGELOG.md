5a0e40ebe4c609db7317f980b62d476514914e7f
Jason Sun <dev.winds@gmail.com>
2023-10-31 15:27:47 +0800

    Add OpenSearch to env list

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,13 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## [1.1.8] - 2023-10-31
+
+### Added
+
+- Add OpenSearch to env list
+
+
 ## [1.1.7] - 2023-10-27
 
 ### Added
--- stdocker/env_handler.py
+++ stdocker/env_handler.py
@@ -129,12 +129,12 @@ class EnvHandler(object):
         customized_envs = self.list_customized_detailed_envs()
         # env_names = self.list_customized_env_names()
         services = ['webserver', 'php', 'mysql', 'mariadb',
-                    'phpmyadmin', 'elasticsearch', 'elasticvue',
+                    'phpmyadmin', 'elasticsearch', 'opensearch', 'elasticvue',
                     'redis', 'phpredisadmin', 'rabbitmq', 'mailcatcher',
                     'mongo', 'mongoexpress', 'postgres', 'pgweb', 'memcached', 'webgrind',
                     'node', 'python', 'golang', 'dotnet', 'java', 'ruby', 'cpp', 'rust']
         service_names = ['Web Server', 'PHP', 'MySQL', 'MariaDB',
-                         'phpMyAdmin', 'Elasticsearch', 'Elasticvue',
+                         'phpMyAdmin', 'Elasticsearch', 'OpenSearch', 'Elasticvue',
                          'Redis', 'phpRedisAdmin', 'RabbitMQ', 'Mailcatcher',
                          'MongoDB', 'Mongo Express', 'PostgreSQL', 'Pgweb', 'Memcached', 'Webgrind',
                          'Node', 'Python', 'Golang', '.Net Core', 'Java', 'Ruby', 'C++', 'Rust']
