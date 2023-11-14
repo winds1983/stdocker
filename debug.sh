0af5c6bce68fbaf983c6561c524017e4049bcd43
Jason Sun <sunf@shinetechsoftware.com>
2023-11-14 13:24:57 +0800

    Fix local install package bug

--- debug.sh
+++ debug.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Uninstall old versions
+# NOTE: Sometimes if we DO NOT remove old versions, the new package is cannot be installed
+sudo pip3 uninstall -y stdocker
+
+# Install local package
 sudo python3 setup.py install
+
+# Show version
 stdocker --version
