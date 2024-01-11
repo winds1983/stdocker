d8aaae9734a6cf4c2db3e014630d0570590436cf
Jason Sun <sunf@shinetechsoftware.com>
2024-01-11 13:33:10 +0800

    Add Stirling PDF, Dynamodb and Dynamodb Admin to env list and Add WooCommerce to PHP platforms

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,14 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## [1.2.8] - 2024-01-11
+
+### Added
+
+- Add Stirling PDF, Dynamodb and Dynamodb Admin to env list
+- Add WooCommerce to PHP platforms
+
+
 ## [1.2.7] - 2024-01-05
 
 ### Fixed
--- stdocker/env_handler.py
+++ stdocker/env_handler.py
@@ -132,12 +132,14 @@ class EnvHandler(object):
                     'phpmyadmin', 'elasticsearch', 'opensearch', 'elasticvue',
                     'redis', 'phpredisadmin', 'rabbitmq', 'mailcatcher',
                     'mongo', 'mongoexpress', 'postgres', 'pgweb', 'memcached', 'webgrind',
-                    'node', 'python', 'golang', 'dotnet', 'java', 'ruby', 'cpp', 'rust', 'mssql', 'sonarqube']
+                    'node', 'python', 'golang', 'dotnet', 'java', 'ruby', 'cpp', 'rust', 'mssql',
+                    'sonarqube', 'stirling_pdf', 'dynamodb', 'dynamodbadmin']
         service_names = ['Web Server', 'PHP', 'MySQL', 'MariaDB',
                          'phpMyAdmin', 'Elasticsearch', 'OpenSearch', 'Elasticvue',
                          'Redis', 'phpRedisAdmin', 'RabbitMQ', 'Mailcatcher',
                          'MongoDB', 'Mongo Express', 'PostgreSQL', 'Pgweb', 'Memcached', 'Webgrind',
-                         'Node', 'Python', 'Golang', '.Net Core', 'Java', 'Ruby', 'C++', 'Rust', 'MSSQL', 'SonarQube']
+                         'Node', 'Python', 'Golang', '.Net Core', 'Java', 'Ruby', 'C++', 'Rust', 'MSSQL',
+                         'SonarQube', 'Stirling PDF', 'Dynamodb', 'Dynamodb Admin']
 
         # Set dynamic max width according to columns of terminal size
         terminal_size = os.get_terminal_size()
